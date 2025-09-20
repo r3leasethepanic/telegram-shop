8d9b433130df71dd335b2d9e9fcb2fdcc83a95f8
r3leasethepanic <168263972+r3leasethepanic@users.noreply.github.com>
2025-09-20 18:36:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -29,4 +29,5 @@ app.get('/api/db-test', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('✅ Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
