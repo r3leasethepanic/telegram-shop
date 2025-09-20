8b91cb12a0b30761f133e88a237ee8f58e348c50
r3leasethepanic <168263972+r3leasethepanic@users.noreply.github.com>
2025-09-20 19:03:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("✅ Backend работает на 3000!");
 });
 
-app.listen(3000, () => {
-  console.log("✅ Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
