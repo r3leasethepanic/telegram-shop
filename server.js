fc604366b329d44f18256dc06ebd64ded250749b
r3leasethepanic <168263972+r3leasethepanic@users.noreply.github.com>
2025-09-20 18:51:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,4 @@ app.get("/", (req, res) => {
 
 // Timeweb ожидает, что сервер слушает process.env.PORT
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+app.listen(PORT, "0.0.0.0", () => console.log(`✅ Running on ${PORT}`));
